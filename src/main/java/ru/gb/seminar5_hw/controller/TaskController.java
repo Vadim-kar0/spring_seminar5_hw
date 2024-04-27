93bfdc2b575707a260f4bfcd1ceb9336e1c7eae7
вадим <funpalka1@gmail.com>
2024-04-27 22:21:41 +0400

    Logger was added

--- src/main/java/ru/gb/seminar5_hw/controller/TaskController.java
+++ src/main/java/ru/gb/seminar5_hw/controller/TaskController.java
@@ -1,6 +1,7 @@
 package ru.gb.seminar5_hw.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.web.bind.annotation.*;
 import ru.gb.seminar5_hw.model.Task;
 import ru.gb.seminar5_hw.model.Task.TaskStatus;
@@ -11,31 +12,40 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/task")
+@Log
 public class TaskController {
     private final TaskService service;
 
     @GetMapping()
     public List<Task> getAllTasks(){
+        log.info("\nUsing getAllTasks: \n" + service.getAllTasks()
+                .toString()
+                .replaceAll("\\), ", "), \n"));
         return service.getAllTasks();
     }
 
     @PostMapping
     public Task addTask(@RequestBody Task task){
+        log.info("Task was created: " + task);
         return service.createTask(task);
     }
 
     @GetMapping("/status/{status}")
     public List<Task> getTasksByStatus(@PathVariable TaskStatus status){
+        log.info("\nGet tasks by status: " + status +  ". \n" + service.getByStatus(status)
+                .toString().replaceAll("\\), ", "), \n"));
         return service.getByStatus(status);
     }
 
     @PutMapping("/{id}")
     public Task updateTaskStatus(@PathVariable Long id,@RequestBody Task task){
+        log.info("Task with id = " + id + " was updated: " + task);
         return service.updateStatusOfTask(id,task.getStatus());
     }
 
     @DeleteMapping("/{id}")
     public void deleteTask(@PathVariable Long id){
+        log.info("Task was deleted: " + service.getTaskById(id));
         service.deleteTask(id);
     }
 
